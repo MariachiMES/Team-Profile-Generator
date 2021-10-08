a54217c341783561c99352620a67621b6beb5ca3
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2021-10-08 13:10:51 -0500

    full functionality achieved, no tests

--- index.js
+++ index.js
@@ -9,40 +9,46 @@ const Engineer = require("./lib/Engineer");
 
 //Array for pushing new team members
 
-const teamMembers = [];
+let teamMembers = [];
 
 //Add manager prompting function
 addManager = () => {
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Enter your manager's Name:",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "what is the manager's email address?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "what is the manager's ID?",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "what is the manager office number?",
-      name: "officeNumber",
-    },
-  ])
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter your manager's Name:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "what is the manager's email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "what is the manager's ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "what is the manager office number?",
+        name: "officeNumber",
+      },
+    ])
 
-  .then((data) => {
-    const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
-    teamMembers.push(manager);
+    .then((data) => {
+      const manager = new Manager(
+        data.name,
+        data.id,
+        data.email,
+        data.officeNumber
+      );
+      teamMembers.push(manager);
 
-    addToTeam();
-  })}
+      addToTeam();
+    });
+};
 
 //Add Engineer function
 
@@ -69,82 +75,114 @@ addEngineer = () => {
         message: "what is the Engineer's GitHub userName?",
         name: "gitHub",
       },
-  
     ])
-  
+
     .then((data) => {
-      const engineer = new Engineer(data.name, data.id, data.email, data.gitHub);
+      const engineer = new Engineer(
+        data.name,
+        data.id,
+        data.email,
+        data.gitHub
+      );
       teamMembers.push(engineer);
-  
+
       addToTeam();
-    })}
+    });
+};
 
 //Add intern prompting function
 
 addIntern = () => {
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "Enter your Intern's Name:",
-            name: "name",
-          },
-          {
-            type: "input",
-            message: "what is the Intern's email address?",
-            name: "email",
-          },
-          {
-            type: "input",
-            message: "what is the Intern's ID?",
-            name: "id",
-          },
-          {
-            type: "input",
-            message: "what school is the Intern attending?",
-            name: "school",
-          },
-      
-        ])
-      
-        .then((data) => {
-          const intern = new Intern(data.name, data.id, data.email, data.school);
-          teamMembers.push(intern);
-      
-          addToTeam();
-        })}
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter your Intern's Name:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "what is the Intern's email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "what is the Intern's ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "what school is the Intern attending?",
+        name: "school",
+      },
+    ])
+
+    .then((data) => {
+      const intern = new Intern(data.name, data.id, data.email, data.school);
+      teamMembers.push(intern);
+
+      addToTeam();
+    });
+};
 
 //Prompt for forking path
 addToTeam = () => {
-      inquirer.prompt([{
-      type: 'list',
-      message: 'Would you like to add a new team member?',
-      name: 'profileOptions',
-      choices: ['Add Engineer', 'Add Intern', 'Generate Team Profile']
-    }
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Would you like to add a new team member?",
+        name: "profileOptions",
+        choices: ["Add Engineer", "Add Intern", "Generate Team Profile"],
+      },
     ])
     .then((data) => {
-    if (data.profileOptions === 'Add Engineer') {
-      addEngineer();
-      } else if (data.profileOptions === 'Add Intern') {
-      addIntern();
-    } else {
-    teamProfileGenerator();
-    }
-  });
-  }
+      if (data.profileOptions === "Add Engineer") {
+        addEngineer();
+      } else if (data.profileOptions === "Add Intern") {
+        addIntern();
+      } else {
+        teamProfileGenerator();
+      }
+    });
+};
 
 //
+teamBuilder = (teamMembers) => {
+  //uses card generator function for each member of team in teamMembers Array
+  let cardHTML = "";
+  teamMembers.forEach(cardGenerator);
 
-
-  .then((response) => writeHTMLfile(response));
-
-var writeHTMLfile = function (response) {
-  
+  function cardGenerator(item) {
+    let employeeInformation = "";
+    if (item.getRole() === "Engineer") {
+      employeeInformation = `GitHub: <a href="https://github.com/${item.gitHub}" target="_blank"> ${item.gitHub}</a>`;
+    } else if (item.getRole() === "Intern") {
+      employeeInformation = `School: ${item.school}`;
+    } else {
+      employeeInformation = `Office #: ${item.officeNumber}`;
+    }
+    cardHTML += `
+<div class="employee-card">
+<div class="card-header" id = "employee-name">
+  ${item.name}
+  <br />
+  <div id = "role">${item.getRole()}</div>
+</div>
+<div class="employee-info">
+  <ul>
+    <li>Employee ID: ${item.id}</li>
+    <li>Email: <a href = "mailto:${item.email}">${item.email}</a></li>
+    <li>${employeeInformation}</li>
+  </ul>
+</div>
+</div>`;
+  }
+  return cardHTML;
 };
 
 teamProfileGenerator = () => {
-
+  console.log(teamMembers);
   const HTML = `<!DOCTYPE html>
   <html lang="en">
     <head>
@@ -159,39 +197,22 @@ teamProfileGenerator = () => {
       <header>
         <nav>My Team</nav>
       </header>
-  
+  <main>
       <section>
 
       ${teamBuilder(teamMembers)}
 
 
       </section>
+      </main>
     </body>
   </html>`;
 
   fs.writeFile("teamProfile.html", HTML, (err) =>
-    err ? console.error(err) : console.log("successfully written teamProfile.html!")
+    err
+      ? console.error(err)
+      : console.log("successfully written teamProfile.html!")
   );
 };
 
-teamBuilder = () => {
-
-
-
-
-}
-
-<div class="employee-card">
-<div class="card-header">
-  ${response.employee}
-  <br />
-  ${response.role}
-</div>
-<div class="employee-info">
-  <ul>
-    <li>${response.id}</li>
-    <li>${response.email}</li>
-    <li>${response.office}</li>
-  </ul>
-</div>
-</div>
+addManager();
